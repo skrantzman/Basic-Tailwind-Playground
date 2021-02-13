8c3bb036eff44ef127e7dcb8ce68ebbb5b4d3fed
Steve Krantzman <skrantzman@comcast.net>
2021-02-13 14:45:37 -0800

    updated TailwindCSS from 1.9.6 to 2.0.3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-	future: {
-		removeDeprecatedGapUtilities: true,
-	},
-	purge: [],
+	purge: [
+		// Uncomment out the two lines below to purge css for production
+		// './src/**/*.html',
+		// './src/**/*.js',
+	  ],
+   
+	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+	  extend: {},
 	},
 	variants: {},
 	plugins: [],
-};
+  }
